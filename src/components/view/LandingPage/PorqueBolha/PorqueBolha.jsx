7beaacc22093b44d4ea9c6573d3acfdcd9cf2678
Victor Martins <VictorMLMartins@icloud.com>
2023-10-23 18:45:49 -0300

    feat: :sparkles: Página LP - Banner Porque Bolha
    Realizado a criação do banner Porque bolha da página Landing Page

--- src/components/view/LandingPage/PorqueBolha/PorqueBolha.jsx
+++ src/components/view/LandingPage/PorqueBolha/PorqueBolha.jsx
@@ -3,35 +3,37 @@ import { PorqueBolhaStyle } from "./porque-bolha.style"
 
 const PorqueBolha = () => {
   return (
-    <PorqueBolhaStyle>
-      <h1>Porque bolha?</h1>
-      <p>Bolha é a rede social que trás algo novo e diferente das redes oferecidas, estore sua bolha e se conecte com o que você mais gosta, escolha  apenas o que você deseja ver em seu feed.</p>
-      <div className="lista-beneficios">
-        <div className="usuarios">
-          <Users size={32} color="#ffffff" weight="thin" />
-          <h3>Usuários</h3>
-          <p>Mais de 1000 usuários</p>
-        </div>
+      <PorqueBolhaStyle>
+        <div className="container">
+          <h1 className='font-1-xxl'>Porque bolha?</h1>
+          <p className="paragrafo-porque font-2-l">Bolha é a rede social que trás algo novo e diferente das redes oferecidas, estore sua bolha e se conecte com o que você mais gosta, escolha  apenas o que você deseja ver em seu feed.</p>
+          <div className="lista-beneficios">
+            <div className="usuarios">
+              <Users size={32} color="#ffffff" weight="thin" />
+              <h3 className="font-1-s">Usuários</h3>
+              <p className="font-1-xs">Mais de 1000 usuários</p>
+            </div>
 
-        <div className="dispositivos">
-          <Devices size={32} color="#ffffff" weight="thin" />
-          <h3>Dispositivos</h3>
-          <p>Desktop, Tablet, Mobile</p>
-        </div>
+            <div className="dispositivos">
+              <Devices size={32} color="#ffffff" weight="thin" />
+              <h3 className="font-1-s">Dispositivos</h3>
+              <p className="font-1-xs">Desktop, Tablet, Mobile</p>
+            </div>
 
-        <div className="seguranca">
-          <ShieldPlus size={32} color="#ffffff" weight="thin" />
-          <h3>Segurança</h3>
-          <p>Mais de 1000 usuários</p>
-        </div>
+            <div className="seguranca">
+              <ShieldPlus size={32} color="#ffffff" weight="thin" />
+              <h3 className="font-1-s">Segurança</h3>
+              <p className="font-1-xs">Mais de 1000 usuários</p>
+            </div>
 
-        <div className="no-ads">
-          <Prohibit size={32} color="#ffffff" weight="thin" />
-          <h3>No Ads</h3>
-          <p>Sem anúncio</p>
+            <div className="no-ads">
+              <Prohibit size={32} color="#ffffff" weight="thin" />
+              <h3 className="font-1-s">No Ads</h3>
+              <p className="font-1-xs">Sem anúncio</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </PorqueBolhaStyle>
+      </PorqueBolhaStyle>
   )
 }
 
