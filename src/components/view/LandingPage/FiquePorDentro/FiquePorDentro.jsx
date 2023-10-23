1c6c4b554dc430b364ec5254a6f4f934e6bdde89
Victor Martins <VictorMLMartins@icloud.com>
2023-10-23 18:44:58 -0300

    feat: :sparkles: Página LP - Banner Fique Por Dentro
    Realizada a criação do banner Fique por dentro da página Landing Page

--- src/components/view/LandingPage/FiquePorDentro/FiquePorDentro.jsx
+++ src/components/view/LandingPage/FiquePorDentro/FiquePorDentro.jsx
@@ -4,9 +4,11 @@ import shape from '/lp-cadastre.png'
 const FiquePorDentro = () => {
   return (
     <FiquePorDentroStyle>
-      <img src={shape} alt="" />
-      <h2>Fique por dentro da sua bolha, entre já na Bubble!</h2>
-      <button>cadastre-se</button>
+      <div className="container">
+        <img src={shape} alt="shape bolha" />
+        <h2 className="font-1-xl">Fique por dentro da sua bolha, entre já na Bubble!</h2>
+        <button>cadastre-se</button>
+      </div>
     </FiquePorDentroStyle>
   )
 }
