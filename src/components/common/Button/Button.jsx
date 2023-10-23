cceec71c0d41fb23e5f7f9af3021fea4de3d4fe3
Renato <renatomna@gmail.com>
2023-10-23 14:44:33 -0300

    feat: :sparkles: terminda a criação do componente button

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -21,24 +21,44 @@ const StylesButton = styled.button`
     justify-content: center;
     align-items: center;
     gap: 10px;
-    border-radius: 8px;
+    cursor: pointer;
 `
 const ButtonCadastrarPrimario = styled(StylesButton)`
-    background-color: ${(props) => props.thema.colors.primary.p3};
+    background-color: ${(props) => props.theme.colors.primary.p3};
+    border-radius: 8px;
+    color: ${(props) => props.theme.colors.black.b100};
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
 `
 const ButtonCadastrarSecundario = styled(StylesButton)`
-    background-color: ${(props) => props.thema.colors.primary.p1};
+    background-color: ${(props) => props.theme.colors.primary.p1};
+    color: ${(props) => props.theme.colors.black.b100};
+    border-radius: 8px;
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+    
 `
 const ButtonForms = styled(StylesButton)`
-    background-color: ${(props) => props.thema.colors.primary.p2};
+    background-color: ${(props) => props.theme.colors.black.b50};
+    color: ${(props) => props.theme.colors.black.b100};
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
 `
 const ButtonPostarOrdenarDescendente = styled(StylesButton)`
     border-radius: 24px;
-    background-color: ${(props) => props.thema.colors.primary.p3};
+    background-color: ${(props) => props.theme.colors.primary.p3};
+    color: ${(props) => props.theme.colors.black.b200};
+    border-radius: 24px;
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
 `
 const ButtonOrdenarAscendente = styled(StylesButton)`
     border-radius: 24px;
-    background-color: ${(props) => props.thema.colors.primary.p2};
+    background-color: ${(props) => props.theme.colors.primary.p2};
+    color: ${(props) => props.theme.colors.black.b200};
+    border-radius: 24px;
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
 `
 
 
